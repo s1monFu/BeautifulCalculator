523c103e823d15ee16dd29fece4a38b70c97656c
s1monFu <laissezfu@gmail.com>
2022-09-02 14:56:48 -0500

    Calculator app implemented dynamic size

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Calculator from "./component/Calculator";
 import Hello from "./component/Hello";
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{"width": "100vw", "height": "100vh", "display": "flex", "justifyContent": "center", "alignItems": "center"}}>
       <Calculator/>
     </div>
   );
--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -1,27 +1,47 @@
 import { useState } from "react";
+import '../styles/buttons.css'
 
 function Calculator() {
-    const [counter, setCounter] = useState(0)
-    const [text, setText] = useState("")
+    // const [counter, setCounter] = useState(0)
+    const [ops, setOps] = useState("")
+    const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+    const pushOperator = (op) => {
+        setOps(ops + op)
+    }
+
+    const calculate = () => {
+        if (ops !== undefined) setOps(eval(ops))
+    }
+
+    const btnSize = window.screen.availableHeight > window.screen.width ? "10vw":"10vh"
+    const displayWidth = window.screen.availableHeight > window.screen.width ? "30vw":"30vh"
+    const displayHeight = window.screen.height > window.screen.width ? "70vw":"70vh"
+
+    console.log(window.screen.width, window.screen.height)
+
+    
+
     return (
-        <div>
-            Counter: {counter}
-            <button onClick={() => { setCounter(counter *10 +1) }}>1</button>
-            <button onClick={() => { setCounter(counter *10 +2) }}>2</button>
-            <button onClick={() => { setCounter(counter *10 +3) }}>3</button>
-            <button onClick={() => { setCounter(counter *10 +4) }}>4</button>
-            <button onClick={() => { setCounter(counter *10 + 5) }}>5</button>
-            <button onClick={() => { setCounter(counter *10 + 6) }}>6</button>
-            <button onClick={() => { setCounter(counter *10 + 7) }}>7</button>
-            <button onClick={() => { setCounter(counter *10 + 8) }}>8</button>
-            <button onClick={() => { setCounter(counter * 10 +9) }}>9</button>
-            <button onClick={() => { setCounter(counter *10) }}>0</button>
-            <button onClick={() => { setCounter(counter *10) }}>+</button>
-            <button onClick={() => { setCounter(counter *10) }}>-</button>
-            <button onClick={() => { setCounter(counter *10) }}>*</button>
-            <button onClick={() => { setCounter(counter *10) }}>/</button>
-            <input onChange={(e) => { setText(e.target.value) }}></input>
-            {/* <p>result: {text}</p> */}
+        <div style={{ "width": displayWidth,"height":displayHeight, "display": "flex", "flexWrap": "wrap", "justifyContent": "center" }}>
+            <div className="display" style={{"width": displayWidth , "height": btnSize}}>{ops}</div>
+
+            {numbers.map((n) =>
+                <button className="numbers" onClick={() => { setOps(ops + n) }} style={{"width" : btnSize, "height": btnSize}}>{n}</button>
+            )}
+            <button className="numbers" onClick={() => { setOps(ops + "0") }} style={{"width": btnSize , "height": btnSize}}>0</button>
+            <button className="numbers" onClick={() => { setOps(ops + ".") }} style={{"width": btnSize , "height": btnSize}}>.</button>
+
+
+            <button className="numbers" onClick={() => { pushOperator("+") }} style={{"width": btnSize , "height": btnSize}}>+</button>
+            <button className="numbers" onClick={() => { pushOperator("-") }} style={{"width": btnSize , "height": btnSize}}>-</button>
+            <button className="numbers" onClick={() => { pushOperator("*") }} style={{"width": btnSize , "height": btnSize}}>*</button>
+            <button className="numbers" onClick={() => { pushOperator("/") }} style={{"width": btnSize , "height": btnSize}}>/</button>
+            <button className="numbers" onClick={() => { calculate()}} style={{"width": btnSize , "height": btnSize}}>=</button>
+
+            <button className="numbers" onClick={() => { setOps(ops.slice(0, -1)) }} style={{"width": btnSize , "height": btnSize}}>Back</button>
+            <button className="numbers" onClick={() => { setOps("") }} style={{"width": btnSize , "height": btnSize}}>Clear</button>
+
 
         </div>
     );
