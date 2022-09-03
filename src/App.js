8f32a73ddf8e83922af747f3c794e921748f2107
s1monFu <laissezfu@gmail.com>
2022-09-02 20:11:27 -0500

    fixed a name inconsistency

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Calculator from "./component/Calculator";
-import Hello from "./component/Hello";
 function App() {
   return (
     <div className="App" style={{"width": "100vw", "height": "100vh", "display": "flex", "justifyContent": "center", "alignItems": "center"}}>
