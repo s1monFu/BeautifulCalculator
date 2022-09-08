24bfa471718d29cbdcee787686924a5f9ce9edfd
s1monFu <laissezfu@gmail.com>
2022-09-07 23:47:07 -0500

    Reformatted

--- src/component/Bar.js
+++ src/component/Bar.js
@@ -4,9 +4,17 @@ import { Link } from "@chakra-ui/react";
 
 const Bar = () => {
   return (
-    <Box display={"flex"} w={"100%"} justifyContent={"space-between"} p={"6"} backgroundColor={"#5591a3"}>
-        <Image boxSize={"50px"} objectFit={"cover"} src="1024.png"></Image>
-      <Text fontSize={"3xl"} fontWeight={"extrabold"} pl={"120"}>BEAUTIFUL CALCULATOR</Text>
+    <Box
+      display={"flex"}
+      w={"100%"}
+      justifyContent={"space-between"}
+      p={"6"}
+      backgroundColor={"#5591a3"}
+    >
+      <Image boxSize={"50px"} objectFit={"cover"} src="1024.png"></Image>
+      <Text fontSize={"3xl"} fontWeight={"extrabold"} pl={"120"}>
+        BEAUTIFUL CALCULATOR
+      </Text>
       <Link href="https://github.com/s1monFu/BeautifulCalculator">
         <Button size="lg" leftIcon={<FaGithub />}>
           Github Page
